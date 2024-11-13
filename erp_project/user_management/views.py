fe3e540854cee8addc661933f42e74f77b360877
kriti <jainkriti583@gmail.com>
2024-11-13 15:44:42 +0530

    user_management_views

--- erp_project/user_management/views.py
+++ erp_project/user_management/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    def get_success_url(self):
+        user_role = self.request.user.role
+        if user_role == 'admin':
+            return '/admin_dashboard/'
+        elif user_role == 'sales_manager':
+            return '/sales_manager_dashboard/'
+        elif user_role == 'hr':
+            return '/hr_dashboard/'
+        elif user_role == 'labour':
+            return '/labour_dashboard/'
+        return super().get_success_url()
